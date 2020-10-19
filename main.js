873e76d907831006df1a0700841b972b226e1031
Gmizavac <47925487+gmiz@users.noreply.github.com>
2020-10-19 18:06:44 +0200

    Removed the dynamic favicon code, fixed favicon again

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// Have to do some more work here, but this should make some nice favicon effects :)
-(function() {
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = 'favicon.ico';
-    document.getElementsByTagName('head')[0].appendChild(link);
-})();
-
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
