5f0e1d8c3bead0808da3adda363a4562a8d5a85e
Gmizavac <47925487+gmiz@users.noreply.github.com>
2020-10-19 13:56:52 +0200

    Fixed the site loading issue

--- main.js
+++ main.js
@@ -6,3 +6,11 @@
     link.href = 'favicon.ico';
     document.getElementsByTagName('head')[0].appendChild(link);
 })();
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
