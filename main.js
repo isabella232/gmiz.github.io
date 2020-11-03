c019a3fa6d1e2cae5cfb058ee41f9d952a530f0e
Gmizavac <47925487+gmiz@users.noreply.github.com>
2020-11-03 07:20:20 +0100

    Many changes. Initial portfolio made. Initial portfolio work.

--- main.js
+++ main.js
@@ -5,3 +5,30 @@ function sleep(milliseconds) {
         currentDate = Date.now();
     } while (currentDate - date < milliseconds);
 }
+
+function oneByOne(classname, start, step) {
+    var cnt = start;
+    var toplinks = document.getElementsByClassName(classname);
+    for (i = 0; i < toplinks.length; i++) {
+        toplinks[i].style.animationDelay = cnt + "s";
+        cnt += step;
+    }
+}
+
+function loadVideo() {
+    var video = document.getElementById('homevid');
+    video.load();
+    video.addEventListener('loadeddata', function() {
+        document.body.classList.remove('js-loading');
+    });
+}
+
+function scrollWithOffset(id, offset){
+    var element = document.getElementById(id);
+    var offsetPosition = element.offsetTop + offset;
+
+    window.scrollTo({
+         top: offsetPosition,
+         behavior: "smooth"
+    });
+}
